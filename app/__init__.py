340d70b4cd4c4b75d23453c6acf8ba7f8c5b215a
Dennis Muvaa <dmuvaa70@gmail.com>
2024-01-29 16:42:40 +0300

    Thesis Corner Initial Commits

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+#login_manager = LoginManager()
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+    db.init_app(app)
+    #login_manager.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    from .auth import routes as auth_routes
+    app.register_blueprint(auth_blueprint)
+
+    from .views import views as views_blueprint
+    from .views import routes as views_routes
+    app.register_blueprint(views_blueprint)
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,47 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# User model
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    orders = db.relationship('Order', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# Order model
+class Order(db.Model):
+    __tablename__ = 'orders'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    deadline = db.Column(db.DateTime, nullable=False)
+    word_count = db.Column(db.Integer)
+    price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='Pending')
+
+    def __repr__(self):
+        return f"Order('{self.title}', '{self.date_posted}')"
+
+# OrderStatus model (optional)
+class OrderStatus(db.Model):
+    
+    __tablename__ = 'order_statuses'
+    id = db.Column(db.Integer, primary_key=True)
+    status_name = db.Column(db.String(50), nullable=False)
+    orders = db.relationship('Order', backref='order_status', lazy=True)
+
+    def __repr__(self):
+        return f"OrderStatus('{self.status_name}')"
