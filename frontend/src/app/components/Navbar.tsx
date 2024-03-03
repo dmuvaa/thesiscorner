e0bbc59754c83441765e6e98a17cef8bd41ec1bd
Domengo <dominicsengo@gmail.com>
2024-03-04 01:53:33 +0300

    Refactor calculator and page components

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
   return (
     <>
       <div>
-        <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-[#6c00c8] from-10% to-[#19d6e1] to-98% p-6">
+        <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-[#6c00c8] from-10% to-[#19d6e1] to-98% px-28 pt-4">
           <div className="flex items-center flex-shrink-0 text-white mr-6">
             <span className="font-semibold text-xl tracking-tight">
               Thesis Corner
