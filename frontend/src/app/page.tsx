8c742f2e77b517f9447cb92f5938c055156c3ae3
Domengo <dominicsengo@gmail.com>
2024-03-04 17:43:43 +0300

    Refactor HeroSection component layout***
    ***Update grid layout in Home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,8 +7,8 @@ import Hero from "./Hero";
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-24 px-4 min-h-screen">
-        <div className="mx-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 items-stretch gap-24 px-4 min-h-screen">
+        <div className="mx-4 self-stretch">
           <Hero />
         </div>
         <div className="p-4">
