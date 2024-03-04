44a31aaa419944dcfa328e4fc1b79b22d248be87
Domengo <dominicsengo@gmail.com>
2024-03-04 17:43:43 +0300

    Refactor HeroSection component layout***
    ***Update grid layout in Home component

--- frontend/src/app/Hero.jsx
+++ frontend/src/app/Hero.jsx
@@ -5,13 +5,17 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="text-white">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold mb-6">
-          Welcome to our Writing Platform
-        </h1>
-        <p className="text-lg mb-10">
-          Order high-quality writing services tailored to your needs.
-        </p>
+      <div className="grid grid-rows-3 grid-flow-col gap-y-8 content-center mx-auto">
+        <div>
+          <h1 className="text-4xl font-bold mb-6">
+            Welcome to our Writing Platform
+          </h1>
+        </div>
+        <div>
+          <p className="text-lg mb-10">
+            Order high-quality writing services tailored to your needs.
+          </p>
+        </div>
         <div className="flex">
           <button className="bg-green-500 hover:bg-green-600 text-white font-bold text-xl py-4 px-8 rounded mr-4">
             Order Now
