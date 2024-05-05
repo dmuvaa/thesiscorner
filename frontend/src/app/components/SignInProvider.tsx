20414a9554343f099dd5864f6b2f8323c4bdb392
Domengo <dominicsengo@mail.com>
2024-05-05 18:04:34 +0300

    Fix import statements and add required attribute to input fields

--- frontend/src/app/components/SignInProvider.tsx
+++ frontend/src/app/components/SignInProvider.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { signProvider } from '../lib/actions';
-import { FaG } from "react-icons/fa6";
+import { GrGoogle} from "react-icons/gr";
 
 const SignInButton: React.FC = () => {
     return (
       <form action={signProvider}>
-        <button type="submit" className="py-3 w-full flex gap-2 border border-slate-300 rounded hover:border-red-500 hover:text-red-400 text-red-500">
-          <FaG className='align-items-center' /> Sign in with Google
+        <button type="submit" className="py-3 w-full flex gap-2 border border-slate-300 rounded hover:border-red-500  justify-center items-center hover:text-red-400 text-red-500">
+          <GrGoogle/>
+          <p className=''>Sign in with Google</p>
         </button>
       </form>
     );
